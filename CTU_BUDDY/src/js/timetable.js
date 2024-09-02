2d4e004213ea122b7aabdb91c0375de947cf92a8
Zaid <zaidgarth19@gmail.com>
2024-09-02 11:05:39 +0200

    Updated the following Luvuyo has to: 1. Add more options for different course 2. Class names for the different day-course 3. Update js by completingn wed to friday 4. Update styling for the drop down box

---
+++ CTU_BUDDY/src/js/timetable.js
@@ -0,0 +1,25 @@
+// Changing Course name
+const course = {
+  programming_foundation: {
+    monday: [
+      "RD412<br/><small>Practical</small>",
+      "RD412<br/><small>Practical</small>",
+      "ENA412<br /><small>Theory</small>",
+      "Group Study",
+      "Group Study",
+      "CW412<br /><small>Practical</small>",
+      "CW412<br /><small>Practical</small>",
+      "CW412<br /><small>Practical</small>",
+    ],
+    tuesday: [
+      "ENA412<br /><small>Theory</small>",
+      "RD412<br /><small>Practical</small>",
+      "RD412<br /><small>Practical</small>",
+      "Research",
+      "",
+      "ENA412<br /><small>Practical</small>",
+      "ENA412<br /><small>Practical</small>",
+      "ENA412<br /><small>Practical</small>",
+    ],
+  },
+};
